2235c4e98ed201f94d6efe6565d429ff20c9fa7c
KimLerbaekPedersen <kilepe@hotmail.com>
2021-06-16 13:18:06 +0200

    Added Slideshow

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var emailVar = document.getElementsByClassName("news4");
+var lnVar = document.getElementById("lname");
 //Get the button
 var mybutton = document.getElementById("myBtn");
     
@@ -17,3 +19,29 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// SLIDESHOW START
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 4000);
+// SLIDESHOW END
+
+/* ======VALIDATE START====== */
+function validateContact(){
+  if(emailVar.value == ""){
+    alert("please fill out first name");
+    return false;
+  }
+  else if(lnVar.value == ""){
+    alert("please fill out last name");
+    return false;
+  }
+}
+/* ======VALIDATE END===== */
+
+
